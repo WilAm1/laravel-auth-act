ee06f14259abd3667ccd173e59fa22e773358013
wil <wilhelmjan.miranda.g@bulsu.edu.ph>
2024-02-12 12:53:02 +0800

    finish: tutorial

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use App\Http\Requests\StoreChirpRequest;
+use App\Http\Requests\UpdateChirpRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        // return view('chirps.index');
+        return view("chirps.index", [
+            'chirps' => Chirp::with('user')->latest()->get()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreChirpRequest $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255'
+        ]);
+
+        $request->user()->chirps()->create($validated);
+        return redirect(route('chirps.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chirp $chirp)
+    {
+        $this->authorize("update", $chirp);
+
+        return view("chirps.edit", compact("chirp"));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateChirpRequest $request, Chirp $chirp)
+    {
+        //
+
+        $this->authorize("update", $chirp);
+
+        $validated = $request->validate([
+            "message" => 'required|string|max:255'
+        ]);
+
+        $chirp->update($validated);
+        return redirect(route("chirps.index"));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chirp $chirp)
+    {
+
+        $this->authorize("delete", $chirp);
+
+        $chirp->delete();
+
+        return redirect(route("chirps.index"));
+    }
+}
